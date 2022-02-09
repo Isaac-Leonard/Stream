96432dfaa74cb13ae0aa5fe7d822d64510ca6a02
Isaac Leonard <isaac.mjs.leonard@gmail.com>
2022-02-10 02:10:24 +1100

    Add most current errors to the errors enum

--- src/errors.rs
+++ src/errors.rs
@@ -3,24 +3,24 @@ pub mod errors {
     use std::ops::Range;
 
     macro_rules! errors {
-        ($(($code:tt, $Name:ident $(($($args:ident)*))?, $msg:tt)),*) => {
+        ($(($code:tt, $Name:ident $(($($arg_name:ident: $arg_type:ty),*))?, $msg:tt)),*) => {
 	    pub enum CompError {
                 $($Name(
-		    $($($args,)*)?
+		    $($($arg_type,)*)?
 			Range<usize>
 		),)*
             }
             impl CompError {
 		fn get_code(&self)->i32{
 		    match self {
-			$(CompError::$Name($($($args,)*)? _) =>$code,)*
+			$(CompError::$Name($($($arg_name,)*)? _) =>$code,)*
 		    }
 		}
                 fn get_msg(&self, lines:&Vec<i32>) -> String {
                     match self {
-			$(CompError::$Name($($($args,)*)? loc) =>{
+			$(CompError::$Name($($($arg_name,)*)? loc) =>{
 			    let pos = get_pos(loc.start as i32, lines);
-			    format!($msg, $($($args,)*)? pos)
+			    format!("Error [{}]: {}, at {}", format!($msg, $($($arg_name,)*)?), self.get_code(), pos)
 			})*
                     }
                 }
@@ -59,13 +59,38 @@ pub mod errors {
     errors!(
         (
             1,
-            ConstReassign(String),
-            "Cannot reassign to already declared constant {} at {}"
+            ConstReassign(name:String),
+            "Cannot reassign to already declared constant '{}'"
         ),
         (
             2,
-            GlobalReassign(String),
-            "Cannot reassign variable in the global scope: '{}' at {}"
-        )
+            GlobalReassign(name:String),
+            "Cannot reassign variable '{}' in the global scope"
+        ),
+        (
+            3,
+            UseUninitialisedVariable(name:String),
+            "Cannot use uninitialised variable '{}'"
+        ),
+        (
+            4,
+            InvalidComparison(op:BinOp, l:CompType, r:CompType),
+            "Invalid operation '{}' for types '{}' and '{}' "
+        ),
+        (5, EmptyType, "Cannot have empty types"),
+	(
+            6,
+            CannotFindVariable(name:String),
+            "Cannot find variable '{}'"
+        ),
+        (7, CannotFindType(ty:String), "Cannot find type '{}'"),
+	(
+            8,
+            BoolInWhile(ty:CompType),
+            "The comparison expression in a while loop must resolve to type 'Bool', found '{}' instead"
+        ),
+	(9, BoolInIf(ty:CompType), "The comparison expression in an if expression must resolve to type  'Bool', found '{}' instead"),
+	(10, InvalidAssignment(allowed:CompType, attempted:CompType), "Type '{}' is not assignable to type '{}'"),
+	(11, NonfunctionCall(name:String, var:CompType), "Cannot call variable '{}' of type '{}'")
     );
 }
