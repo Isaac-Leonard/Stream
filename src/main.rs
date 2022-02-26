a280c9d3855cc93f8186a10048cd4b46bbbfb9da
Isaac Leonard <isaac.mjs.leonard@gmail.com>
2022-02-26 17:30:56 +1100

    Refactor main function to make compiling multiple files at once easier

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,19 @@ use parser::*;
 use settings::Settings;
 use shared::*;
 use std::{collections::HashMap, env, fs};
-fn main() {
-    let src = fs::read_to_string(env::args().nth(1).expect("Expected file argument"))
-        .expect("Failed to read file");
-    let newlines_positions = src.split("\n").map(|x| x.len()).collect::<Vec<_>>();
+
+fn calc_lines(file: &str) -> Vec<i32> {
+    let newlines_positions = file.split('\n').map(|x| x.len()).collect::<Vec<_>>();
     let mut positions = vec![0];
-    let mut last = 0 as i32;
+    let mut last = 0;
     for pos in newlines_positions {
         last += pos as i32 + 1;
         positions.push(last);
     }
-    let args = env::args().collect::<Vec<_>>();
-    let settings = Settings {
-        print_llvm: args.contains(&"-p".to_string()),
-        skip_optimizations: !args.contains(&"-s".to_string()),
-    };
-    // let src = "[!]+";
+    positions
+}
+
+fn get_global_scope() -> CompScope {
     let mut types = HashMap::new();
     types.insert("Int".to_string(), CompType::Int);
     types.insert("Float".to_string(), CompType::Float);
@@ -34,11 +31,18 @@ fn main() {
     types.insert("Bool".to_string(), CompType::Bool);
     types.insert("Null".to_string(), CompType::Null);
     let variables = HashMap::new();
-    let global_scope = CompScope {
+    CompScope {
         variables,
         types,
         parent: None,
-    };
+    }
+}
+
+fn compile_file(name: &str, settings: Settings) {
+    let src = fs::read_to_string(name).expect("Failed to read file");
+
+    let lines = calc_lines(&src);
+    let global_scope = get_global_scope();
     let parsed = parser().parse(src.trim());
     match parsed {
         Ok(ast) => {
@@ -47,9 +51,19 @@ fn main() {
                 Ok(prog) => compile::compile(&prog, settings),
                 Err(messages) => messages
                     .into_iter()
-                    .for_each(|e| println!("{}", e.get_msg(&positions))),
+                    .for_each(|e| println!("{}", e.get_msg(&lines))),
             };
         }
         Err(errs) => errs.into_iter().for_each(|e| println!("{:?}", e)),
     }
+}
+
+fn main() {
+    let name = env::args().nth(1).expect("Expected file argument");
+    let args = env::args().collect::<Vec<_>>();
+    let settings = Settings {
+        print_llvm: args.contains(&"-p".to_string()),
+        skip_optimizations: !args.contains(&"-s".to_string()),
+    };
+    compile_file(&name, settings);
 }
