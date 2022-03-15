1ffa6062f7040bea352da04b2ca6c01943af9d5f
Isaac Leonard <isaac.mjs.leonard@gmail.com>
2022-03-16 10:46:01 +1100

    Make typeof less greedy so it doesn't need to be wrapped in brackets

--- src/parser.rs
+++ src/parser.rs
@@ -182,19 +182,19 @@ fn exp_parser<'a>() -> impl Parser<char, Expression, Error = Cheap<char>> + 'a {
             .delimited_by('{', '}')
             .map_with_span(Struct);
 
-        let typeof_check = keyword("typeof")
-            .then(whitespace())
-            .ignore_then(exp.clone().map(Box::new))
-            .map_with_span(Typeof)
-            .boxed();
-
         let primary_exp = func_call
-            .or(typeof_check.clone())
             .or(symbol_parser().map_with_span(Terminal))
             .or(func_declaration.map_with_span(Terminal))
             .or(exp.clone().padded().delimited_by('(', ')'))
             .boxed()
             .labelled("Primary");
+
+        let typeof_check = keyword("typeof")
+            .then(whitespace())
+            .ignore_then(primary_exp.clone().map(Box::new))
+            .map_with_span(Typeof)
+            .boxed();
+        let primary_exp = typeof_check.clone().or(primary_exp.clone());
 
         let mult_parser = (primary_exp.clone())
             .then(
@@ -302,7 +302,6 @@ fn exp_parser<'a>() -> impl Parser<char, Expression, Error = Cheap<char>> + 'a {
 
         let expression = struct_exp
             .or(if_parser)
-            .or(typeof_check)
             .or(loop_parser)
             .or(reassign)
             .or(type_declaration.clone())
