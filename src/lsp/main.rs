eca47057fab22136998107fa78b8e4b622364781
Isaac Leonard <isaac.mjs.leonard@gmail.com>
2022-04-11 03:58:12 +1000

    Initial running server

---
+++ src/lsp/main.rs
@@ -0,0 +1,529 @@
+#[path = "../ast.rs"]
+pub mod ast;
+pub mod completion;
+#[path = "../errors.rs"]
+pub mod errors;
+#[path = "../lexer.rs"]
+mod lexer;
+#[path = "../macros.rs"]
+pub mod macros;
+#[path = "../parser.rs"]
+pub mod parser;
+pub mod reference;
+pub mod semantic_token;
+#[path = "../settings.rs"]
+pub mod settings;
+#[path = "../shared.rs"]
+pub mod shared;
+use crate::shared::create_program;
+use ast::*;
+use chumsky::Parser;
+use log::debug;
+use semantic_token::semantic_token_from_ast;
+use semantic_token::ImCompleteSemanticToken;
+use semantic_token::LEGEND_TYPE;
+use settings::Settings;
+use std::collections::HashMap;
+
+use dashmap::DashMap;
+use ropey::Rope;
+use serde_json::Value;
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+    ast_map: DashMap<String, ExpEnvironment>,
+    document_map: DashMap<String, Rope>,
+    semantic_token_map: DashMap<String, Vec<ImCompleteSemanticToken>>,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            server_info: None,
+            capabilities: ServerCapabilities {
+                text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                    TextDocumentSyncKind::FULL,
+                )),
+                completion_provider: Some(CompletionOptions {
+                    resolve_provider: Some(false),
+                    trigger_characters: Some(vec![".".to_string()]),
+                    work_done_progress_options: Default::default(),
+                    all_commit_characters: None,
+                }),
+                execute_command_provider: Some(ExecuteCommandOptions {
+                    commands: vec!["dummy.do_something".to_string()],
+                    work_done_progress_options: Default::default(),
+                }),
+
+                workspace: Some(WorkspaceServerCapabilities {
+                    workspace_folders: Some(WorkspaceFoldersServerCapabilities {
+                        supported: Some(true),
+                        change_notifications: Some(OneOf::Left(true)),
+                    }),
+                    file_operations: None,
+                }),
+                semantic_tokens_provider: Some(
+                    SemanticTokensServerCapabilities::SemanticTokensRegistrationOptions(
+                        SemanticTokensRegistrationOptions {
+                            text_document_registration_options: {
+                                TextDocumentRegistrationOptions {
+                                    document_selector: Some(vec![DocumentFilter {
+                                        language: Some("nrs".to_string()),
+                                        scheme: Some("file".to_string()),
+                                        pattern: None,
+                                    }]),
+                                }
+                            },
+                            semantic_tokens_options: SemanticTokensOptions {
+                                work_done_progress_options: WorkDoneProgressOptions::default(),
+                                legend: SemanticTokensLegend {
+                                    token_types: LEGEND_TYPE.clone().into(),
+                                    token_modifiers: vec![],
+                                },
+                                range: Some(true),
+                                full: Some(SemanticTokensFullOptions::Bool(true)),
+                            },
+                            static_registration_options: StaticRegistrationOptions::default(),
+                        },
+                    ),
+                ),
+                // definition: Some(GotoCapability::default()),
+                definition_provider: Some(OneOf::Left(true)),
+                references_provider: Some(OneOf::Left(true)),
+                rename_provider: Some(OneOf::Left(true)),
+                ..ServerCapabilities::default()
+            },
+        })
+    }
+    async fn semantic_tokens_full(
+        &self,
+        params: SemanticTokensParams,
+    ) -> Result<Option<SemanticTokensResult>> {
+        let uri = params.text_document.uri.to_string();
+        self.client
+            .log_message(MessageType::LOG, "semantic_token_full")
+            .await;
+        let semantic_tokens = || -> Option<Vec<SemanticToken>> {
+            let mut im_complete_tokens = self.semantic_token_map.get_mut(&uri)?;
+            let rope = self.document_map.get(&uri)?;
+            let ast = self.ast_map.get(&uri)?;
+            let extends_tokens = semantic_token_from_ast(&ast);
+            im_complete_tokens.extend(extends_tokens);
+            im_complete_tokens.sort_by(|a, b| a.start.cmp(&b.start));
+            let mut pre_line = 0;
+            let mut pre_start = 0;
+            let semantic_tokens = im_complete_tokens
+                .iter()
+                .filter_map(|token| {
+                    let line = rope.try_byte_to_line(token.start as usize).ok()? as u32;
+                    let first = rope.try_line_to_char(line as usize).ok()? as u32;
+                    let start = rope.try_byte_to_char(token.start as usize).ok()? as u32 - first;
+                    let delta_line = line - pre_line;
+                    let delta_start = if delta_line == 0 {
+                        start - pre_start
+                    } else {
+                        start
+                    };
+                    let ret = Some(SemanticToken {
+                        delta_line,
+                        delta_start,
+                        length: token.length as u32,
+                        token_type: token.token_type as u32,
+                        token_modifiers_bitset: 0,
+                    });
+                    pre_line = line;
+                    pre_start = start;
+                    ret
+                })
+                .collect::<Vec<_>>();
+            Some(semantic_tokens)
+        }();
+        if let Some(semantic_token) = semantic_tokens {
+            return Ok(Some(SemanticTokensResult::Tokens(SemanticTokens {
+                result_id: None,
+                data: semantic_token,
+            })));
+        }
+        Ok(None)
+    }
+
+    async fn semantic_tokens_range(
+        &self,
+        params: SemanticTokensRangeParams,
+    ) -> Result<Option<SemanticTokensRangeResult>> {
+        let uri = params.text_document.uri.to_string();
+        let semantic_tokens = || -> Option<Vec<SemanticToken>> {
+            let im_complete_tokens = self.semantic_token_map.get(&uri)?;
+            let rope = self.document_map.get(&uri)?;
+            let mut pre_line = 0;
+            let mut pre_start = 0;
+            let semantic_tokens = im_complete_tokens
+                .iter()
+                .filter_map(|token| {
+                    let line = rope.try_byte_to_line(token.start as usize).ok()? as u32;
+                    let first = rope.try_line_to_char(line as usize).ok()? as u32;
+                    let start = rope.try_byte_to_char(token.start as usize).ok()? as u32 - first;
+                    let ret = Some(SemanticToken {
+                        delta_line: line - pre_line,
+                        delta_start: if start >= pre_start {
+                            start - pre_start
+                        } else {
+                            start
+                        },
+                        length: token.length as u32,
+                        token_type: token.token_type as u32,
+                        token_modifiers_bitset: 0,
+                    });
+                    pre_line = line;
+                    pre_start = start;
+                    ret
+                })
+                .collect::<Vec<_>>();
+            Some(semantic_tokens)
+        }();
+        if let Some(semantic_token) = semantic_tokens {
+            return Ok(Some(SemanticTokensRangeResult::Tokens(SemanticTokens {
+                result_id: None,
+                data: semantic_token,
+            })));
+        }
+        Ok(None)
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "initialized!")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+
+    async fn references(&self, params: ReferenceParams) -> Result<Option<Vec<Location>>> {
+        Ok(Some(Vec::new()))
+    }
+
+    async fn goto_definition(
+        &self,
+        params: GotoDefinitionParams,
+    ) -> Result<Option<GotoDefinitionResponse>> {
+        let definition = || -> Option<GotoDefinitionResponse> {
+            let uri = params.text_document_position_params.text_document.uri;
+            let ast = self.ast_map.get(&uri.to_string())?;
+            let rope = self.document_map.get(&uri.to_string())?;
+
+            let position = params.text_document_position_params.position;
+            let char = rope.try_line_to_char(position.line as usize).ok()?;
+            let offset = char + position.character as usize;
+            let span = Some((0, 0..0));
+            span.and_then(|(_, range)| {
+                let start_position = offset_to_position(range.start, &rope)?;
+                let end_position = offset_to_position(range.end, &rope)?;
+
+                let range = Range::new(start_position, end_position);
+
+                Some(GotoDefinitionResponse::Scalar(Location::new(uri, range)))
+            })
+        }();
+        Ok(definition)
+    }
+    async fn did_change_workspace_folders(&self, _: DidChangeWorkspaceFoldersParams) {
+        self.client
+            .log_message(MessageType::INFO, "workspace folders changed!")
+            .await;
+    }
+
+    async fn did_change_configuration(&self, _: DidChangeConfigurationParams) {
+        self.client
+            .log_message(MessageType::INFO, "configuration changed!")
+            .await;
+    }
+
+    async fn did_change_watched_files(&self, _: DidChangeWatchedFilesParams) {
+        self.client
+            .log_message(MessageType::INFO, "watched files have changed!")
+            .await;
+    }
+
+    async fn execute_command(&self, _: ExecuteCommandParams) -> Result<Option<Value>> {
+        self.client
+            .log_message(MessageType::INFO, "command executed!")
+            .await;
+
+        match self.client.apply_edit(WorkspaceEdit::default()).await {
+            Ok(res) if res.applied => self.client.log_message(MessageType::INFO, "applied").await,
+            Ok(_) => self.client.log_message(MessageType::INFO, "rejected").await,
+            Err(err) => self.client.log_message(MessageType::ERROR, err).await,
+        }
+
+        Ok(None)
+    }
+
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file opened!")
+            .await;
+        self.on_change(TextDocumentItem {
+            uri: params.text_document.uri,
+            text: params.text_document.text,
+            version: params.text_document.version,
+        })
+        .await
+    }
+
+    async fn did_change(&self, mut params: DidChangeTextDocumentParams) {
+        self.on_change(TextDocumentItem {
+            uri: params.text_document.uri,
+            text: std::mem::take(&mut params.content_changes[0].text),
+            version: params.text_document.version,
+        })
+        .await
+    }
+
+    async fn did_save(&self, _: DidSaveTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file saved!")
+            .await;
+    }
+
+    async fn did_close(&self, _: DidCloseTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file closed!")
+            .await;
+    }
+
+    async fn rename(&self, params: RenameParams) -> Result<Option<WorkspaceEdit>> {
+        Ok(None)
+    }
+
+    async fn completion(&self, params: CompletionParams) -> Result<Option<CompletionResponse>> {
+        let uri = params.text_document_position.text_document.uri;
+        let position = params.text_document_position.position;
+        let completions = || -> Option<Vec<CompletionItem>> {
+            let rope = self.document_map.get(&uri.to_string())?;
+            let ast = self.ast_map.get(&uri.to_string())?;
+            let char = rope.try_line_to_char(position.line as usize).ok()?;
+            let offset = char + position.character as usize;
+            let completions = completion::completion(&ast, offset);
+            let mut ret = Vec::with_capacity(completions.len());
+            for (_, item) in completions {
+                match item {
+                    completion::ImCompleteCompletionItem::Variable(var) => {
+                        ret.push(CompletionItem {
+                            label: var.clone(),
+                            insert_text: Some(var.clone()),
+                            kind: Some(CompletionItemKind::VARIABLE),
+                            detail: Some(var),
+                            ..Default::default()
+                        });
+                    }
+                    completion::ImCompleteCompletionItem::Function(name, args) => {
+                        ret.push(CompletionItem {
+                            label: name.clone(),
+                            kind: Some(CompletionItemKind::FUNCTION),
+                            detail: Some(name.clone()),
+                            insert_text: Some(format!(
+                                "{}({})",
+                                name,
+                                args.iter()
+                                    .enumerate()
+                                    .map(|(index, item)| { format!("${{{}:{}}}", index + 1, item) })
+                                    .collect::<Vec<_>>()
+                                    .join(",")
+                            )),
+                            insert_text_format: Some(InsertTextFormat::SNIPPET),
+                            ..Default::default()
+                        });
+                    }
+                }
+            }
+            Some(ret)
+        }();
+        Ok(completions.map(CompletionResponse::Array))
+    }
+}
+struct TextDocumentItem {
+    uri: Url,
+    text: String,
+    version: i32,
+}
+impl Backend {
+    async fn on_change(&self, params: TextDocumentItem) {
+        let rope = ropey::Rope::from_str(&params.text);
+        self.document_map
+            .insert(params.uri.to_string(), rope.clone());
+        let (ast, errors, semantic_tokens) = parse(create_settings(&params));
+        self.client
+            .log_message(MessageType::INFO, format!("{:?}", errors))
+            .await;
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::build(|client| Backend {
+        client,
+        ast_map: DashMap::new(),
+        document_map: DashMap::new(),
+        semantic_token_map: DashMap::new(),
+    })
+    .finish();
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
+
+fn offset_to_position(offset: usize, rope: &Rope) -> Option<Position> {
+    let line = rope.try_char_to_line(offset).ok()?;
+    let first_char = rope.try_line_to_char(line).ok()?;
+    let column = offset - first_char;
+    Some(Position::new(line as u32, column as u32))
+}
+
+fn parse(
+    settings: Settings,
+) -> (
+    ExpEnvironment,
+    Vec<chumsky::error::Cheap<String>>,
+    Vec<ImCompleteSemanticToken>,
+) {
+    let name = settings.input_name.clone();
+    let mut files = parse_files(settings, HashMap::new());
+    transform_files(&name, &mut files);
+    (
+        files
+            .get(&name)
+            .as_ref()
+            .unwrap()
+            .program
+            .as_ref()
+            .unwrap()
+            .body
+            .clone(),
+        Vec::new(),
+        Vec::new(),
+    )
+}
+
+fn parse_files(
+    settings: Settings,
+    mut files: HashMap<String, ImportMap>,
+) -> HashMap<String, ImportMap> {
+    let src = std::fs::read_to_string(&settings.input_name).expect("Failed to read file");
+    let lines = calc_lines(&src);
+    let tokens = lexer::lexer()
+        .parse(src.trim())
+        .map_err(|x| panic!("{:?}", x))
+        .unwrap();
+    let len = tokens.len();
+    let parsed = crate::parser::parser().parse(chumsky::stream::Stream::from_iter(
+        len..len + 1,
+        tokens.into_iter(),
+    ));
+    match parsed {
+        Ok(ast) => {
+            for import in &ast.0 {
+                if !files.contains_key(&import.file) {
+                    let sub_settings = Settings {
+                        call_linker: false,
+                        input_name: import.file.clone(),
+                        object_name: import.file.replace(".bs", ".o"),
+                        ..settings
+                    };
+                    files = parse_files(sub_settings, files);
+                }
+            }
+            files.insert(
+                settings.input_name.clone(),
+                ImportMap {
+                    file: settings.input_name.clone(),
+                    settings,
+                    line_numbers: lines,
+                    depends_on: ast.0,
+                    ast: Some(ast.1),
+                    program: None,
+                    compiled: false,
+                },
+            );
+            files
+        }
+        Err(errs) => {
+            errs.into_iter().for_each(|e| println!("{:?}", e));
+            files
+        }
+    }
+}
+fn calc_lines(file: &str) -> Vec<i32> {
+    let newlines_positions = file.split('\n').map(|x| x.len()).collect::<Vec<_>>();
+    let mut positions = vec![0];
+    let mut last = 0;
+    for pos in newlines_positions {
+        last += pos as i32 + 1;
+        positions.push(last);
+    }
+    positions
+}
+struct ImportMap {
+    //TODO: Change string to path
+    file: String,
+    depends_on: Vec<ImportFrom>,
+
+    program: Option<Program>,
+    ast: Option<(std::ops::Range<usize>, ast::Expression)>,
+    compiled: bool,
+    settings: Settings,
+    line_numbers: Vec<i32>,
+}
+
+fn create_settings(document: &TextDocumentItem) -> Settings {
+    Settings {
+        print_llvm: false,
+        skip_optimizations: true,
+        call_linker: false,
+        input_name: document.uri.path().to_string(),
+        object_name: String::new(),
+    }
+}
+
+fn transform_files(name: &str, programs: &mut HashMap<String, ImportMap>) {
+    let mut global_scope = get_global_scope();
+    for import in programs.get(name).unwrap().depends_on.clone() {
+        transform_files(&import.file, programs);
+        if let Some(prog) = &programs.get(&import.file).unwrap().program {
+            println!("here");
+            prog.get_exported().iter().for_each(|x| {
+                global_scope.variables.insert(x.name.clone(), x.clone());
+            });
+        }
+    }
+    let mut program = programs.get_mut(name).unwrap();
+    let prog = create_program(program.ast.as_ref().unwrap(), &mut global_scope);
+    if let Ok(prog) = prog {
+        program.program = Some(prog);
+    }
+}
+
+fn get_global_scope() -> TempScope {
+    let mut types = HashMap::new();
+    types.insert("Int".to_string(), CompType::Int);
+    types.insert("Float".to_string(), CompType::Float);
+    types.insert("Ptr".to_string(), CompType::Ptr);
+    types.insert("Bool".to_string(), CompType::Bool);
+    types.insert("Null".to_string(), CompType::Null);
+    let variables = HashMap::new();
+    TempScope {
+        preset_variables: HashMap::new(),
+        variables,
+        types,
+        parent: None,
+    }
+}
---
+++ src/lsp/reference.rs
@@ -0,0 +1,122 @@
+use crate::ast::*;
+use std::ops::Range;
+
+use im_rc::Vector;
+
+#[derive(Debug, Clone)]
+pub enum ReferenceSymbol {
+    Founded((String, Range<usize>)),
+    Founding(usize),
+}
+use ReferenceSymbol::*;
+pub fn get_reference(
+    ast: &ExpEnvironment,
+    ident_offset: usize,
+    include_self: bool,
+) -> Vec<(String, Range<usize>)> {
+    let mut reference_list = vec![];
+    // for (_, v) in ast.iter() {
+    //     if v.name.1.end < ident_offset {
+    //         vector.push_back(v.name.clone());
+    //     }
+    // }
+    let mut kv_list = ast;
+    let mut reference_symbol = ReferenceSymbol::Founding(ident_offset);
+    // let mut fn_vector = Vector::new();
+    reference_list
+}
+
+pub fn get_reference_of_expr(
+    expr: &ExpEnvironment,
+    definition_ass_list: Vector<(String, Range<usize>)>,
+    reference_symbol: ReferenceSymbol,
+    reference_list: &mut Vec<(String, Range<usize>)>,
+    include_self: bool,
+) {
+    use CompExpression::*;
+    match &expr.expression.as_ref() {
+        Value(_) => {}
+        Read(name) => {
+            if let Founded((symbol_name, symbol_span)) = reference_symbol {
+                if symbol_name == name.name {
+                    let index = definition_ass_list
+                        .iter()
+                        .position(|decl| decl.0 == symbol_name);
+                    if let Some(symbol) = index.map(|i| definition_ass_list.get(i).unwrap().clone())
+                    {
+                        if symbol == (symbol_name.to_string(), symbol_span) {
+                            reference_list.push((name.name.clone(), expr.located.clone()));
+                        }
+                    };
+                }
+            }
+            // if ident_offset >= local.1.start && ident_offset < local.1.end {
+            //     let index = definition_ass_list
+            //         .iter()
+            //         .position(|decl| decl.0 == local.0);
+            //     (
+            //         false,
+            //         index.map(|i| definition_ass_list.get(i).unwrap().clone()),
+            //     )
+            // } else {
+            //     (true, None)
+            // }
+        }
+        BinOp(op, lhs, rhs) => {
+            get_reference_of_expr(
+                lhs,
+                definition_ass_list.clone(),
+                reference_symbol.clone(),
+                reference_list,
+                include_self,
+            );
+            get_reference_of_expr(
+                rhs,
+                definition_ass_list.clone(),
+                reference_symbol,
+                reference_list,
+                include_self,
+            );
+        }
+        Call(callee, args) => {}
+        IfElse {
+            cond,
+            then,
+            otherwise,
+        } => {
+            get_reference_of_expr(
+                cond,
+                definition_ass_list.clone(),
+                reference_symbol.clone(),
+                reference_list,
+                include_self,
+            );
+            get_reference_of_expr(
+                cond,
+                definition_ass_list.clone(),
+                reference_symbol.clone(),
+                reference_list,
+                include_self,
+            );
+            get_reference_of_expr(
+                otherwise,
+                definition_ass_list,
+                reference_symbol.clone(),
+                reference_list,
+                include_self,
+            );
+        }
+        List(lst) => {
+            for expr in lst {
+                get_reference_of_expr(
+                    expr,
+                    definition_ass_list.clone(),
+                    reference_symbol.clone(),
+                    reference_list,
+                    include_self,
+                );
+            }
+        }
+        _ => {}
+    }
+}
