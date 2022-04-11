ce966bc3f3bc2d8cada92a2cb168330271643741
Isaac Leonard <isaac.mjs.leonard@gmail.com>
2022-04-12 00:29:55 +1000

    Refactor the lsp module to import stuff rather then copy and pasting

--- src/lsp/main.rs
+++ src/lsp/main.rs
@@ -10,15 +10,15 @@ pub mod macros;
 #[path = "../parser.rs"]
 pub mod parser;
 pub mod reference;
+#[path = "../runner.rs"]
+pub mod runner;
 pub mod semantic_token;
 #[path = "../settings.rs"]
 pub mod settings;
 #[path = "../shared.rs"]
 pub mod shared;
-use crate::shared::create_program;
+use crate::runner::*;
 use ast::*;
-use chumsky::Parser;
-use log::debug;
 use semantic_token::semantic_token_from_ast;
 use semantic_token::ImCompleteSemanticToken;
 use semantic_token::LEGEND_TYPE;
@@ -413,77 +413,6 @@ fn parse(
         Vec::new(),
     )
 }
-
-fn parse_files(
-    settings: Settings,
-    mut files: HashMap<String, ImportMap>,
-) -> HashMap<String, ImportMap> {
-    let src = std::fs::read_to_string(&settings.input_name).expect("Failed to read file");
-    let lines = calc_lines(&src);
-    let tokens = lexer::lexer()
-        .parse(src.trim())
-        .map_err(|x| panic!("{:?}", x))
-        .unwrap();
-    let len = tokens.len();
-    let parsed = crate::parser::parser().parse(chumsky::stream::Stream::from_iter(
-        len..len + 1,
-        tokens.into_iter(),
-    ));
-    match parsed {
-        Ok(ast) => {
-            for import in &ast.0 {
-                if !files.contains_key(&import.file) {
-                    let sub_settings = Settings {
-                        call_linker: false,
-                        input_name: import.file.clone(),
-                        object_name: import.file.replace(".bs", ".o"),
-                        ..settings
-                    };
-                    files = parse_files(sub_settings, files);
-                }
-            }
-            files.insert(
-                settings.input_name.clone(),
-                ImportMap {
-                    file: settings.input_name.clone(),
-                    settings,
-                    line_numbers: lines,
-                    depends_on: ast.0,
-                    ast: Some(ast.1),
-                    program: None,
-                    compiled: false,
-                },
-            );
-            files
-        }
-        Err(errs) => {
-            errs.into_iter().for_each(|e| println!("{:?}", e));
-            files
-        }
-    }
-}
-fn calc_lines(file: &str) -> Vec<i32> {
-    let newlines_positions = file.split('\n').map(|x| x.len()).collect::<Vec<_>>();
-    let mut positions = vec![0];
-    let mut last = 0;
-    for pos in newlines_positions {
-        last += pos as i32 + 1;
-        positions.push(last);
-    }
-    positions
-}
-struct ImportMap {
-    //TODO: Change string to path
-    file: String,
-    depends_on: Vec<ImportFrom>,
-
-    program: Option<Program>,
-    ast: Option<(std::ops::Range<usize>, ast::Expression)>,
-    compiled: bool,
-    settings: Settings,
-    line_numbers: Vec<i32>,
-}
-
 fn create_settings(document: &TextDocumentItem) -> Settings {
     Settings {
         print_llvm: false,
@@ -491,39 +420,5 @@ fn create_settings(document: &TextDocumentItem) -> Settings {
         call_linker: false,
         input_name: document.uri.path().to_string(),
         object_name: String::new(),
-    }
-}
-
-fn transform_files(name: &str, programs: &mut HashMap<String, ImportMap>) {
-    let mut global_scope = get_global_scope();
-    for import in programs.get(name).unwrap().depends_on.clone() {
-        transform_files(&import.file, programs);
-        if let Some(prog) = &programs.get(&import.file).unwrap().program {
-            println!("here");
-            prog.get_exported().iter().for_each(|x| {
-                global_scope.variables.insert(x.name.clone(), x.clone());
-            });
-        }
-    }
-    let mut program = programs.get_mut(name).unwrap();
-    let prog = create_program(program.ast.as_ref().unwrap(), &mut global_scope);
-    if let Ok(prog) = prog {
-        program.program = Some(prog);
-    }
-}
-
-fn get_global_scope() -> TempScope {
-    let mut types = HashMap::new();
-    types.insert("Int".to_string(), CompType::Int);
-    types.insert("Float".to_string(), CompType::Float);
-    types.insert("Ptr".to_string(), CompType::Ptr);
-    types.insert("Bool".to_string(), CompType::Bool);
-    types.insert("Null".to_string(), CompType::Null);
-    let variables = HashMap::new();
-    TempScope {
-        preset_variables: HashMap::new(),
-        variables,
-        types,
-        parent: None,
     }
 }
