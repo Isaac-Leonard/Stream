6679e10f1ed99e70ca88ef5e163cae67b54c1c62
Isaac Leonard <isaac.mjs.leonard@gmail.com>
2022-05-26 13:03:22 +1000

    Make the linker use clang with default arguments instead of platform and system dependent lld configurations

--- src/linker.rs
+++ src/linker.rs
@@ -10,43 +10,16 @@ pub struct Linker {
 }
 impl Linker {
     pub fn link(&self) -> Result<(), String> {
-        if os == MACOS {
-            let mut command = Command::new("ld64.lld");
-            if self.demangle {
-                command.arg("-demangle");
-            }
-            command
-                .arg("-lto_library")
-                .arg("/usr/local/Cellar/llvm/13.0.0_2/lib/libLTO.dylib");
-            if self.dynamic {
-                command.arg("-dynamic");
-            }
-            command
-                .arg("-arch")
-                .arg("x86_64")
-                .arg("-platform_version")
-                .arg("macos")
-                .arg("11.0.0")
-                .arg("11.0.0")
-                .arg("-syslibroot")
-                .arg("/Library/Developer/CommandLineTools/SDKs/MacOSX11.sdk")
-                .arg("-o")
-                .arg(&self.output_file);
-            for file in &self.input_files {
-                command.arg(file);
-            }
-            command
-                .arg("-lSystem")
-                .arg(
-                    "/usr/local/Cellar/llvm/13.0.1_1/lib/clang/13.0.1/lib/darwin/libclang_rt.osx.a",
-                )
-                .output()
-                .map_err(|x| format!("{:?}", x))
-                .map(|x| String::from_utf8(x.stderr).expect("utf8 error"))
-                .and_then(|x| if x.is_empty() { Ok(()) } else { Err(x) })
-        } else {
-            Err(format!("Automatic linking not supported on this operating system '{}', please feel free to help contribute a patch",os ))
+        let mut command = Command::new("clang");
+        command.arg("-o").arg(&self.output_file);
+        for file in &self.input_files {
+            command.arg(file);
         }
+        command
+            .output()
+            .map_err(|x| format!("{:?}", x))
+            .map(|x| String::from_utf8(x.stderr).expect("utf8 error"))
+            .and_then(|x| if x.is_empty() { Ok(()) } else { Err(x) })
     }
 
     pub fn new() -> Self {
